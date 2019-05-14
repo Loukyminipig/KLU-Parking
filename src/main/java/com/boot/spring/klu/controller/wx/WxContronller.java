75ad6037e9ffb94114cc0f806506b1a3f31cdfb6
Loukyminipig <623594876@qq.com>
2019-05-14 19:41:20 +0800

    添加微信访问控制器和路由

--- src/main/java/com/boot/spring/klu/controller/wx/WxContronller.java
+++ src/main/java/com/boot/spring/klu/controller/wx/WxContronller.java
@@ -1,5 +1,21 @@
 package com.boot.spring.klu.controller.wx;
 
-public class WxContronller {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/wx")
+public class WxContronller {
+	
+	
+	/**
+	 * @auth liweipeng 2019年5月14日下午7:37:28
+	 * @param
+	 * @return String
+	 * @description 测试微信访问端口
+	 */
+	@RequestMapping("/welcome")
+	public String welcome() {
+		return "welcome";
+	}
 }
---
+++ src/main/java/com/boot/spring/klu/mapper/WxMapper.java
@@ -0,0 +1,13 @@
+package com.boot.spring.klu.mapper;
+
+import org.springframework.stereotype.Repository;
+
+/**
+*@auth liweipeng 
+*@version 2019年5月14日下午7:36:11
+**/
+
+@Repository
+public interface WxMapper {
+
+}
